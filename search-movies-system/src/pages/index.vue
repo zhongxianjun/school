f8586edc0266402a4ca13da95612378f59481cec
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-12-10 21:38:34 +0800

    第一天

---
+++ search-movies-system/src/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="index">
+    <h1 class="logo">欢迎进入"你想看"电影世界</h1>
+    <div class="main">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+/*lang="sass"*/
+.index .logo{
+  font-size: 28px;
+  background: linear-gradient(to left, pink, transparent );
+}
+.main{
+  width: 850px;
+  min-height: 100vh;
+  background: #efeee6;
+  margin: 0 auto;
+}
+</style>
