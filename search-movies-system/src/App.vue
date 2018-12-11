3cbc84a741b45b5dee4b76bedf970cf09229c2d5
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-12-11 19:17:46 +0800

    首页制作完成

--- search-movies-system/src/App.vue
+++ search-movies-system/src/App.vue
@@ -18,5 +18,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #efeee6;
+  font-family: KaiTi;
 }
+*{ margin:0px; padding:0px;}
+a { text-decoration:none; blr:expression(this.onFocus=this.blur()); outline:none;}
+a:focus{ outline:none;}
+img{ border:none;}
+li{list-style:none;}
 </style>
--- search-movies-system/src/pages/index.vue
+++ search-movies-system/src/pages/index.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="index">
     <h1 class="logo">欢迎进入"你想看"电影世界</h1>
+    <hr>
     <div class="main">
-      
+      <div class="containe">
+        <div class="header">
+          <input type="text" name="search" v-model="val" placeholder="请输入电影相关信息进行搜索...">
+          <i @click="search"></i>
+        </div>
+        <div class="history">
+          <h2 class="title">历史记录</h2>
+          <div class="list">
+            <div class="li" v-for="(item, index) in history" @click="searchHistory(item.name)">
+              {{item.name}}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +26,45 @@ export default {
   name: 'index',
   data () {
     return {
-      
+      val: "",
+      history: [
+        {name: "海贼王剧场版"},
+        {name: "极限特工"},
+        {name: "绝地求生"},
+        {name: "黑客帝国"},
+        {name: "三体1"},
+        {name: "三体2"},
+        {name: "三体3"},
+        {name: "异形4"},
+      ]
+    }
+  },
+  methods: {
+    search(){
+       let val = this.val;
+       let count = 0;
+       let _this = this;
+
+       this.history.map((item, index)=>{
+        
+        if(val == item.name){
+          _this.history.splice(index,1);
+          count++;
+        }
+       })
+       if(val !== "" && count == 0){
+
+        this.history.unshift({name: val});
+        count = 0;
+
+       }else if(count > 0){
+         this.history.unshift({name: val});
+       }
+    },
+
+    searchHistory(item){
+      this.val = item;
+      this.search();
     }
   }
 }
@@ -21,14 +73,80 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  scoped>
 /*lang="sass"*/
+.index{
+  min-height: 100vh;
+}
 .index .logo{
   font-size: 28px;
-  background: linear-gradient(to left, pink, transparent );
+  font-weight: normal;
+  background: #cffbd085;
+  padding: 8px 0;
 }
-.main{
-  width: 850px;
-  min-height: 100vh;
-  background: #efeee6;
+hr{
+  background: #fff;
+}
+.index .main{
+  width: 100%;
+  min-height: calc(100vh - 58px);
+  background: linear-gradient(to bottom, #cffbd085, #cffbd085);
   margin: 0 auto;
+  padding-top: 8px;
+}
+.index .main .containe{
+  max-width: 340px;
+  margin: 0 auto;
+}
+.index .main .header{
+  width: 340px;
+  margin: 0 auto;
+  display: flex;
+
+}
+.index .main .header input{
+  width: 300px;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 8px;
+  border: 1px solid #fff;
+  border-radius: 6px;
+  font-size: 16px;
+  outline: none;
+}
+.index .main .header i{
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  border: 1px solid #fff;
+  border-radius: 6px;
+  text-align: center;
+  background: url(./../assets/images/search.png) center center no-repeat;
+  background-size: 24px;
+  background-color: #fff;
+}
+
+.index .main .history{
+  margin-top: 28px;
+  text-align: left;
+}
+.index .main .history .title{
+  color: #000;
+  font-weight: normal;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+.index .main .history .list{
+  display: flex;
+  flex-wrap: wrap;
+}
+.index .main .history .list .li{
+  font-size: 14px;
+  color: #aaa85;
+  width: 32%;
+  height: 28px;
+  line-height: 28px;
+  text-align: center;
+  border-radius: 12px;
+  background: #ddd;
+  margin: 6px 2px;
 }
 </style>
